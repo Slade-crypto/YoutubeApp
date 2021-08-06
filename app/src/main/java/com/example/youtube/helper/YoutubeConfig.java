e8044e22dd9cfddb35e538d5fd52ba167c8aca59
Slade-crypto <renan.feliciano.rocha@gmail.com>
2021-08-06 11:16:02 -0300

    Configurado as bibliotecas Retrofit e Youtube no app

---
+++ app/src/main/java/com/example/youtube/helper/YoutubeConfig.java
@@ -0,0 +1,9 @@
+package com.example.youtube.helper;
+
+public class YoutubeConfig {
+
+    public static String CANAL_ID = "";
+    public static String CHAVE_YOUTUBE_API ="AIzaSyCMxY79sFccH_t87xs4z5Rrzj4lFH8F8Rs";
+    public static String URL_BASE = "";
+
+}
