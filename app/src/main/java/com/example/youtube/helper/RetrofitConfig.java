c8faf4cf95b5a474f0c25aeb1fafd8f650a51ada
Slade-crypto <renan.feliciano.rocha@gmail.com>
2021-08-06 12:12:21 -0300

    introduzindo à API do youtube, url base adquirida e Id do canal também adquirida

--- app/src/main/java/com/example/youtube/helper/RetrofitConfig.java
+++ app/src/main/java/com/example/youtube/helper/RetrofitConfig.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitConfig {
-    
+
     public static Retrofit getRetrofit(){
         return new Retrofit.Builder()
                 .baseUrl(YoutubeConfig.URL_BASE)
--- app/src/main/java/com/example/youtube/helper/YoutubeConfig.java
+++ app/src/main/java/com/example/youtube/helper/YoutubeConfig.java
@@ -2,8 +2,8 @@ package com.example.youtube.helper;
 
 public class YoutubeConfig {
 
-    public static String CANAL_ID = "";
+    public static String CANAL_ID = "UCAjbAPiBK_LIHoh1OlrXrPg";
     public static String CHAVE_YOUTUBE_API ="AIzaSyCMxY79sFccH_t87xs4z5Rrzj4lFH8F8Rs";
-    public static String URL_BASE = "";
+    public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 }
